9b25075c62f627493d2cfc26792a0053f273cb4a
MMMac <elementarcreations@gmail.com>
2025-06-08 11:53:37 +0200

    added migrations, sorry for being bit late

---
+++ KolokwiumAPBD/Migrations/20250608095206_InitialCreate.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KolokwiumAPBD.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Program",
+                columns: table => new
+                {
+                    ProgramId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DurationMinutes = table.Column<int>(type: "int", nullable: false),
+                    TemperatureCelcius = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Program", x => x.ProgramId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WashingMachines",
+                columns: table => new
+                {
+                    WashingMachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaxWeight = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    SerialNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WashingMachines", x => x.WashingMachineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Available_Program",
+                columns: table => new
+                {
+                    AvailableProgramId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WashingMachineId = table.Column<int>(type: "int", nullable: false),
+                    ProgramId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Available_Program", x => x.AvailableProgramId);
+                    table.ForeignKey(
+                        name: "FK_Available_Program_Program_ProgramId",
+                        column: x => x.ProgramId,
+                        principalTable: "Program",
+                        principalColumn: "ProgramId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Available_Program_WashingMachines_WashingMachineId",
+                        column: x => x.WashingMachineId,
+                        principalTable: "WashingMachines",
+                        principalColumn: "WashingMachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Purchase_History",
+                columns: table => new
+                {
+                    AvailableProgramId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchase_History", x => new { x.AvailableProgramId, x.CustomerId });
+                    table.ForeignKey(
+                        name: "FK_Purchase_History_Available_Program_AvailableProgramId",
+                        column: x => x.AvailableProgramId,
+                        principalTable: "Available_Program",
+                        principalColumn: "AvailableProgramId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Purchase_History_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1, "Maciej", "Mesjasz", "+48794287703" });
+
+            migrationBuilder.InsertData(
+                table: "Program",
+                columns: new[] { "ProgramId", "DurationMinutes", "Name", "TemperatureCelcius" },
+                values: new object[,]
+                {
+                    { 1, 69, "Quickie", 30 },
+                    { 2, 143, "CottonDestroyer", 90 },
+                    { 3, 90, "Synthetic", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "WashingMachines",
+                columns: new[] { "WashingMachineId", "MaxWeight", "SerialNumber" },
+                values: new object[,]
+                {
+                    { 1, 32.12m, "APBD1111/S123/15" },
+                    { 2, 55.23m, "APBD1112/S123/24" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Available_Program",
+                columns: new[] { "AvailableProgramId", "Price", "ProgramId", "WashingMachineId" },
+                values: new object[,]
+                {
+                    { 1, 33.4m, 1, 1 },
+                    { 2, 48.7m, 2, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Purchase_History",
+                columns: new[] { "AvailableProgramId", "CustomerId", "PurchaseDate", "Rating" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2025, 6, 8, 10, 0, 0, 0, DateTimeKind.Unspecified), 5 },
+                    { 2, 1, new DateTime(2025, 6, 8, 11, 0, 0, 0, DateTimeKind.Unspecified), null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Available_Program_ProgramId",
+                table: "Available_Program",
+                column: "ProgramId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Available_Program_WashingMachineId",
+                table: "Available_Program",
+                column: "WashingMachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchase_History_CustomerId",
+                table: "Purchase_History",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchase_History");
+
+            migrationBuilder.DropTable(
+                name: "Available_Program");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Program");
+
+            migrationBuilder.DropTable(
+                name: "WashingMachines");
+        }
+    }
+}
